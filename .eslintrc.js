d9b849f7324155b1ecfba18a5fea8402f79a16d3
Yan-Fa Li <yanfali@gmail.com>
2020-03-03 10:29:47 -0800

    Update to Vue CLI 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,20 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  env: {
+    node: true
+  },
+  plugins: ['cypress'],
+  extends: [
+    'plugin:vue/essential',
+    '@vue/prettier',
+    'plugin:cypress/recommended'
+  ],
+  rules: {
+    'cypress/no-unnecessary-waiting': 'off',
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'google',
-    'plugin:vue/recommended',
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-
   }
-}
+};
